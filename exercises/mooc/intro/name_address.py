52997933bf2d05b89bf683fa389e14ab1fc5da56
anamar696-netizen <anamar696@gmail.com>
2025-10-22 14:10:18 +0200

    Testing Ana commit

--- exercises/mooc/intro/name_address.py
+++ exercises/mooc/intro/name_address.py
@@ -3,7 +3,6 @@
 # Steve Sanders
 # 91 Station Road
 # London EC05 6AW
-# Write your solution here
 given_name = input("What is your name?")
 family_name = input("What is your family name?")
 address = input("What is your street address?")
